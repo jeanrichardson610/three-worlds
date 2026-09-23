515bdd1777cf67be5fd90358d47d8155dd9ef28d
jeanrichardson610 <jean.richardson610@gmail.com>
2026-09-22 21:01:44 -0700

    missed a word

--- src/components/weather/RadarMap.tsx
+++ src/components/weather/RadarMap.tsx
@@ -6,7 +6,7 @@ import type { GeoResult } from "@/types/weather";
 import { fetchRadarFrames, radarTileUrl, type RadarFrame } from "@/lib/rainviewerapi";
 
 const DARK_TILES =
-  `https://basemaps.cartocdn.com/rastertiles/dark_all/{z}/{x}/{y}.png?key=${import.meta.env.CARTO_API_KEY}`;
+  `https://basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}.png?key=${import.meta.env.CARTO_API_KEY}`;
 const TILE_ATTRIBUTION =
   '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>';
 
