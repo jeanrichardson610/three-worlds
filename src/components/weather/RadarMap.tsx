11609d76736625e44ea0de9959866dc5bff8c950
jeanrichardson610 <jean.richardson610@gmail.com>
2026-09-22 21:20:54 -0700

    testing with a direct key

--- src/components/weather/RadarMap.tsx
+++ src/components/weather/RadarMap.tsx
@@ -6,7 +6,7 @@ import type { GeoResult } from "@/types/weather";
 import { fetchRadarFrames, radarTileUrl, type RadarFrame } from "@/lib/rainviewerapi";
 
 const DARK_TILES =
-  `https://basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}.png?api_key=${import.meta.env.CARTO_API_KEY}`;
+  `https://basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}.png?key=cb1_3ulg_1_55124853e39b81f4e1077d74`;
 const TILE_ATTRIBUTION =
   '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>, &copy; <a href="https://carto.com/attributions">CARTO</a>';
 
