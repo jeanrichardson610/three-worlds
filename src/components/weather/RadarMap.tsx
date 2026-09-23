fb9348a91692cb0eeb9a29245c6434db7da5608d
jeanrichardson610 <jean.richardson610@gmail.com>
2026-09-22 20:41:41 -0700

    Updating key name

--- src/components/weather/RadarMap.tsx
+++ src/components/weather/RadarMap.tsx
@@ -6,7 +6,7 @@ import type { GeoResult } from "@/types/weather";
 import { fetchRadarFrames, radarTileUrl, type RadarFrame } from "@/lib/rainviewerapi";
 
 const DARK_TILES =
-  "https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png?api_key=${import.meta.env.VITE_CARTO_API_KEY}";
+  "https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png?api_key=${import.meta.env.CARTO_API_KEY}";
 const TILE_ATTRIBUTION =
   '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>';
 
